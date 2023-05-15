c5ecaffdffe246c7226560a2282716e43f56b16f
magicalbeauty91 <magicalbeauty91@gmail.com>
2023-05-15 01:49:29 -0500

    added hero section with background image and styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,57 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from "../assets/img/header-img.svg"
+import headerImg from "../assets/img/me.png"
 
 const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["MERN Full stack Developer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+        
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
   return (
       <div className='banner' id='home'>
           <Container>
               <Row className="align-items-center">
                   <Col xs={12} md={6} xl={7}>
                       <span className='tagline'>Welcome to my Portfolio</span>
-                      <h1>{`Hi, I'm Marissa`}<span className='wrap'> web developer</span></h1>
+                      <h1>{`Hi, I'm Marissa `}<span className='wrap'>{text}</span></h1>
                       <p>Hello, boys and girls. Today we're gonna talk about father and daughter relationship. Do you have a daddy? I bet you do. Who's your daddy?</p>
                       <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                   </Col>
                   <Col xs={12} md={6} xl={5}>
-                      <img src={headerImg} alt="Header Img" />
+                      <img className="mern" src={headerImg} alt="Header Img" />
                   </Col>
               </Row>
           </Container>
