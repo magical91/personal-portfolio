847ec77fce812f53978c81f96d41c896b099222e
magicalbeauty91 <magicalbeauty91@gmail.com>
2023-05-14 19:22:43 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar.js"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
