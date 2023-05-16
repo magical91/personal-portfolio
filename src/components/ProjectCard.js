61ee097aa1f5c212df32a221cb7ea867bfc27bec
magicalbeauty91 <magicalbeauty91@gmail.com>
2023-05-16 17:35:09 -0500

    fixed projectCard rendering problems

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,15 +2,15 @@ import { Col } from "react-bootstrap";
 
 const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-      <Col sm={6} md={4}>
+      <Col size={12} sm={6} md={4}>
           <div className="proj-imgbx">
-              <img src={imgUrl} alt="project example for my portfolio" />
+              <img src={imgUrl} className="projectImg" />
               <div className="proj-txt">
                   <h4>{title}</h4>
                   <span>{description}</span>
               </div>
           </div>
-    </Col>
+      </Col>
   )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
-import projectImg1 from "../assets/img/Photo-coming-soon.jpg";
-import projectImg2 from "../assets/img/Placeholder.jpg";
-import projectImg3 from "../assets/img/inderdeep-kanda.jpg";
+import projImg1 from "../assets/img/Photo-coming-soon.jpg";
+import projImg2 from "../assets/img/Placeholder.jpg";
+import projImg3 from "../assets/img/inderdeep-kanda.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png"
 import ProjectCard from "./ProjectCard";
 import { Container, Nav, Row, Col, Tab } from "react-bootstrap"
@@ -10,63 +10,70 @@ const Projects = () => {
 
     const projects = [
         {
-            title: "Example 1",
-            description: "Web Development",
-            imgUrl: projectImg1,
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
         },
         {
-            title: "Example 2",
-            description: "Web Development",
-            imgUrl: projectImg2,
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
         },
         {
-            title: "Example 3",
-            description: "Web Development",
-            imgUrl: projectImg3,
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
         }
-    ]
+    ];
 
-  return (
-      <section className="project" id="project">
-          <Container>
-              <Row>
-                  <Col>
-                      <h2>Projects</h2>
-                      <p>This will be a my projects description in the near future</p>
-                      <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" defaultActiveKey="/home">
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <h2>Projects</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
                                 </Nav.Item>
-                          </Nav>
-                          <Tab.Content>
-                              <Tab.Pane event="first">
-                                  <Row>
-                                      {
-                                          projects.map((project, index) => {
-                                              return (
-                                                  <ProjectCard key={index} {...project} />
-                                          )
-                                         }) 
-                                      }
-                                  </Row>
-                              </Tab.Pane>
-                              <Tab.Pane event="second">I'm gay</Tab.Pane>
-                              <Tab.Pane event="third">Bitch me too</Tab.Pane>
-                          </Tab.Content>
-                      </Tab.Container>
-                  </Col>
-              </Row>
-          </Container>
-          <img className="background-image-right" src={colorSharp2} alt="gradient background for my portfolio"></img>
-    </section>
-  )
+                            </Nav>
+                            <Tab.Content id="slideInUp" >
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="section">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+        </section>
+    )
 }
 
-export default Projects
+export default Projects;
